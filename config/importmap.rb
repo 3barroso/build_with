4635884befa08c4ef3c35f14f7108e07b72b577b
Erik <erikbarroso22@gmail.com>
2024-03-17 15:27:22 -0400

    adds dartsass, builds and installs. does not seem compatible with bootstrap? (requires sassc-rails - requires sprockets => overrides propshaft?)

--- config/importmap.rb
+++ config/importmap.rb
@@ -5,3 +5,7 @@ pin "@hotwired/turbo-rails", to: "turbo.min.js"
 pin "@hotwired/stimulus", to: "stimulus.min.js"
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
+
+# needs gems bootstrap and sass-rails? (this adds sprockets as dependency which then overrides Propshaft? — requires manifest.js)
+# pin "bootstrap" # @5.3.3
+# pin "@popperjs/core", to: "@popperjs--core.js" # @2.11.8
