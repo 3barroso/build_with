376ebc3adc1b632d15ebfc3a7f5f702422f21c3b
Erik <erikbarroso22@gmail.com>
2024-03-18 00:42:19 -0400

    adds company scaffolding and sets company index as root

--- config/importmap.rb
+++ config/importmap.rb
@@ -7,5 +7,5 @@ pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
 
 # needs gems bootstrap and sass-rails? (this adds sprockets as dependency which then overrides Propshaft? — requires manifest.js)
-# pin "bootstrap" # @5.3.3
-# pin "@popperjs/core", to: "@popperjs--core.js" # @2.11.8
+pin "bootstrap", to: "https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css"
+pin "@popperjs/core", to: "https://cdnjs.cloudflare.com/ajax/libs/popper.js/2.9.2/umd/popper.min.js"
