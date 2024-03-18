f4ea5373ddea1ce42aedd481c720f2a4b35118a9
Erik <erikbarroso22@gmail.com>
2024-03-18 01:06:23 -0400

    adds bootstrap to applcation layout to use as quick css

--- config/importmap.rb
+++ config/importmap.rb
@@ -6,6 +6,6 @@ pin "@hotwired/stimulus", to: "stimulus.min.js"
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
 
-# needs gems bootstrap and sass-rails? (this adds sprockets as dependency which then overrides Propshaft? — requires manifest.js)
-pin "bootstrap", to: "https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css"
-pin "@popperjs/core", to: "https://cdnjs.cloudflare.com/ajax/libs/popper.js/2.9.2/umd/popper.min.js"
+# Able to add bootstrap css on application.html.erb with CDN in <head>
+# pin "bootstrap", to: "https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" # @5.3.2
+# pin "@popperjs/core", to: "https://cdnjs.cloudflare.com/ajax/libs/popper.js/2.9.2/umd/popper.min.js" # @2.11.8
