427a0aeb9c825dde16875fba57cd0f91be6befd2
Erik <erikbarroso22@gmail.com>
2024-04-23 16:57:24 -0400

    adds slug string to company as unique optional param. changes routes to use slug within company. catches all as base path and searches for company

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,19 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
   resources :adventures
   resources :addresses
-  resources :companies
+  resources :companies, param: :slug
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
+  get 'up' => 'rails/health#show', as: :rails_health_check
 
   # Defines the root path route ("/")
-  # root "posts#index"
-  root "companies#index"  
+  root 'companies#index'
+
+  get '*slug', to: 'companies#show', constraints: lambda { |req|
+    req.path.exclude? 'rails/active_storage'
+  }
 end
--- db/structure.sql
+++ db/structure.sql
@@ -1,6 +1,6 @@
 CREATE TABLE IF NOT EXISTS "schema_migrations" ("version" varchar NOT NULL PRIMARY KEY);
 CREATE TABLE IF NOT EXISTS "ar_internal_metadata" ("key" varchar NOT NULL PRIMARY KEY, "value" varchar, "created_at" datetime(6) NOT NULL, "updated_at" datetime(6) NOT NULL);
-CREATE TABLE IF NOT EXISTS "companies" ("id" integer PRIMARY KEY AUTOINCREMENT NOT NULL, "name" varchar, "created_at" datetime(6) NOT NULL, "updated_at" datetime(6) NOT NULL);
+CREATE TABLE IF NOT EXISTS "companies" ("id" integer PRIMARY KEY AUTOINCREMENT NOT NULL, "name" varchar, "created_at" datetime(6) NOT NULL, "updated_at" datetime(6) NOT NULL, "slug" varchar);
 CREATE TABLE sqlite_sequence(name,seq);
 CREATE TABLE IF NOT EXISTS "addresses" ("id" integer PRIMARY KEY AUTOINCREMENT NOT NULL, "street" varchar, "postal_code" varchar, "street_number" varchar, "city" varchar, "state" varchar, "fips_code" varchar, "country" varchar, "created_at" datetime(6) NOT NULL, "updated_at" datetime(6) NOT NULL, "location_type" integer, "county" varchar);
 CREATE UNIQUE INDEX "index_addresses_on_street_and_street_number_and_city_and_state" ON "addresses" ("street", "street_number", "city", "state");
@@ -21,7 +21,9 @@ CREATE TABLE IF NOT EXISTS "adventures" ("id" integer PRIMARY KEY AUTOINCREMENT 
 CREATE TABLE IF NOT EXISTS "adventure_addresses" ("id" integer PRIMARY KEY AUTOINCREMENT NOT NULL, "address_id" integer, "adventure_id" integer, "name" varchar, "description" text, "created_at" datetime(6) NOT NULL, "updated_at" datetime(6) NOT NULL);
 CREATE INDEX "index_adventure_addresses_on_address_id" ON "adventure_addresses" ("address_id");
 CREATE INDEX "index_adventure_addresses_on_adventure_id" ON "adventure_addresses" ("adventure_id");
+CREATE UNIQUE INDEX "index_companies_on_slug" ON "companies" ("slug");
 INSERT INTO "schema_migrations" (version) VALUES
+('20240423194134'),
 ('20240417190607'),
 ('20240411182921'),
 ('20240325003334'),
