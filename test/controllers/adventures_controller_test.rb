f01ebb41742f343a1c5104e8e28b04208771e06d
Erik <erikbarroso22@gmail.com>
2024-04-24 13:09:57 -0400

    rubocop frozen string magic comment and single quote preferance change

--- test/controllers/adventures_controller_test.rb
+++ test/controllers/adventures_controller_test.rb
@@ -1,4 +1,6 @@
-require "test_helper"
+# frozen_string_literal: true
+
+require 'test_helper'
 
 class AdventuresControllerTest < ActionDispatch::IntegrationTest
   setup do
@@ -6,53 +8,53 @@ class AdventuresControllerTest < ActionDispatch::IntegrationTest
     @address = addresses(:one)
   end
 
-  test "should get index" do
+  test 'should get index' do
     get adventures_url
     assert_response :success
   end
 
-  test "should get new" do
+  test 'should get new' do
     get new_adventure_url
     assert_response :success
   end
 
-  test "should create adventure" do
-    assert_difference("Adventure.count") do
+  test 'should create adventure' do
+    assert_difference('Adventure.count') do
       post adventures_url, params: { adventure: { active: @adventure.active, description: @adventure.description, shareable: @adventure.shareable, status: @adventure.status, target_completion: @adventure.target_completion, title: @adventure.title } }
     end
 
     assert_redirected_to adventure_url(Adventure.last)
   end
 
-  test "should create addresses with adventure creation" do
+  test 'should create addresses with adventure creation' do
     assert_difference ['Address.count', 'Adventure.count'], 1 do
-      post adventures_url, params: { adventure: { active: @adventure.active, "adventure_addresses_attributes"=>{"0"=>{"description"=>"home reno", "address_attributes"=>{"city"=>@address.city, "street_number"=>"123", "street"=>@address.street, "state"=>@address.state }}}}}
+      post adventures_url, params: { adventure: { active: @adventure.active, 'adventure_addresses_attributes'=>{'0'=>{'description'=>'home reno', 'address_attributes'=>{'city'=>@address.city, 'street_number'=>'123', 'street'=>@address.street, 'state'=>@address.state }}}}}
     end
   end
 
-  test "should find exisiting addresses with adventure creation" do
+  test 'should find exisiting addresses with adventure creation' do
     assert_difference -> { Address.count } => 0, -> { Adventure.count } => 1 do
-      post adventures_url, params: { adventure: { active: @adventure.active, "adventure_addresses_attributes"=>{"0"=>{"description"=>"home reno", "address_attributes"=>{"city"=>@address.city, "street_number"=>@address.street_number, "street"=>@address.street, "state"=>@address.state }}}}}
+      post adventures_url, params: { adventure: { active: @adventure.active, 'adventure_addresses_attributes'=>{'0'=>{'description'=>'home reno', 'address_attributes'=>{'city'=>@address.city, 'street_number'=>@address.street_number, 'street'=>@address.street, 'state'=>@address.state }}}}}
     end
   end
 
-  test "should show adventure" do
+  test 'should show adventure' do
     get adventure_url(@adventure)
     assert_response :success
   end
 
-  test "should get edit" do
+  test 'should get edit' do
     get edit_adventure_url(@adventure)
     assert_response :success
   end
 
-  test "should update adventure" do
+  test 'should update adventure' do
     patch adventure_url(@adventure), params: { adventure: { active: @adventure.active, description: @adventure.description, shareable: @adventure.shareable, status: @adventure.status, target_completion: @adventure.target_completion, title: @adventure.title } }
     assert_redirected_to adventure_url(@adventure)
   end
 
-  test "should destroy adventure" do
-    assert_difference("Adventure.count", -1) do
+  test 'should destroy adventure' do
+    assert_difference('Adventure.count', -1) do
       delete adventure_url(@adventure)
     end
 
