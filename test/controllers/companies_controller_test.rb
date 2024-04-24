91b30a8eb6746ebdc674cd21e2ce30e7c5436273
Erik <erikbarroso22@gmail.com>
2024-04-24 12:54:37 -0400

    adds coverage to company slug uniqueness check during creation or update

--- test/controllers/companies_controller_test.rb
+++ test/controllers/companies_controller_test.rb
@@ -3,6 +3,7 @@ require "test_helper"
 class CompaniesControllerTest < ActionDispatch::IntegrationTest
   setup do
     @company = companies(:one)
+    @company2 = companies(:two)
   end
 
   test "should get index" do
@@ -23,6 +24,14 @@ class CompaniesControllerTest < ActionDispatch::IntegrationTest
     assert_redirected_to company_url(Company.last)
   end
 
+  test "should check unique slug before creating company" do
+    assert_no_changes("Company.count") do
+      post companies_url, params: { company: { name: @company.name, slug: @company2.slug } }
+    end
+
+    assert_response :unprocessable_entity
+  end
+
   test "should show company" do
     get company_url(@company.slug)
     assert_response :success
@@ -36,6 +45,11 @@ class CompaniesControllerTest < ActionDispatch::IntegrationTest
   test "should update company" do
     patch company_url(@company.slug), params: { company: { name: @company.name } }
     assert_redirected_to company_url(@company)
+  end
+
+  test "should check unique slug before updating company" do
+    patch company_url(@company.slug), params: { company: { name: @company.name, slug: @company2.slug } }
+    assert_response :unprocessable_entity
   end
 
   test "should destroy company" do
